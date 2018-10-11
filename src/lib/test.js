4d5aaedfe6cd12d9c952ee5ed9e654024a9f2d95
Maria Klimova <masha-klimova@inbox.ru>
2018-10-11 20:48:10 +0300

    hw1

---
+++ src/lib/test.js
@@ -0,0 +1,15 @@
+var say = function (name) {
+    var div = document.createElement("div");
+    div.style.width = "100px";
+    div.style.height = "100px";
+    div.style.background = "red";
+    div.style.color = "white";
+    div.innerHTML = "gg";
+
+    document.body.appendChild(div);
+    //return say();   
+    
+};
+
+export default say;
+
