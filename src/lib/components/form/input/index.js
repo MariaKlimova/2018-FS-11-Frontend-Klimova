267b53695a7974f90b325eb76196da1ed2c0e86b
Maria Klimova <masha-klimova@inbox.ru>
2018-10-12 11:59:18 +0300

    hw1

---
+++ src/lib/components/form/input/index.js
@@ -0,0 +1,10 @@
+import shadowStyles from '.shadow.css';
+
+const template = `
+    <style>${shadowStyles.toString()}</style>
+    <input />
+    <slot name = "icon"></slot>
+`;
+
+class FormInput extends HTMLElement {
+    construct///ssylka v telege
