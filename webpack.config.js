19f7bb19ff131157eec76544e8a8e8ebc1301797
Maria Klimova <masha-klimova@inbox.ru>
2018-10-17 21:58:10 +0300

    ESlint eroors fixed
    Styles in css

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js',
+    },
+
+    module:  {
+    rules: [ 
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+   
+     {
+      test: /\.css/,
+      use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+}
+
